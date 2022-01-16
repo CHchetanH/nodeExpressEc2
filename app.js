dab2b69e7149ce12fe2c7f6da736535e784f32d6
Chetan Hatnapure <chchetanrules7@gmail.com>
2022-01-16 22:34:05 +0530

    second

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
 	console.log("listening on Port 3000");
+});
+
+app.get("/", (req, res) => {
+	res.json({name: "Chetan", message: "Hello from Node"});
 });
